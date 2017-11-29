891451d9c43dcb5860c4c1229ad2606b39d1f76e
moxbro <blueraven93@gmail.com>
2017-11-29 11:01:35 +0100

    deatils solved

--- src/pages/show-diseases/show-diseases.ts
+++ src/pages/show-diseases/show-diseases.ts
@@ -18,20 +18,29 @@ export class ShowDiseasesPage {
 
   private options = { name: "DbDiagnosis.db", location: 'default', createFromLocation: 1 };
   private queryNames = "SELECT  DiseaseName, DiseaseEval, DiseaseInfoNames, SymptomGroup, Valid, DiseaseInfoValue FROM ChosenSynonymsDiseases;";
-  public diseaseNames: String[] = [];
-  public chosenSymptoms: String[] = [];
-  public randomText = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,";
-  //public diseases = [];
+  //public diseaseNames: String[] = [];
+  //public chosenSymptoms: String[] = [];
+  diseaseNames: Array<{title: string, details: string, icon: string, showDetails: boolean}> = [];
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private sqlite: SQLite) {
     this.sqlite.create(this.options).then((db: SQLiteObject) => {
       db.executeSql(this.queryNames, {}).then((data) => {
         let rows = data.rows;
         for (let i = 0; i < rows.length; i++)
-          this.diseaseNames.push(rows.item(i).DiseaseName);
+          this.diseaseNames.push(
+            {
+            title: rows.item(i).DiseaseName,
+            details: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+            icon: 'ios-add-circle-outline',
+            showDetails: false
+            }
+          );
         console.log("ShowDiseases-Load: " + JSON.stringify(data));
       })
       //this.names.push(this.names.length.toString());
+
+
+
     });
 
     //this.navParams.get('chosenSymptoms');
@@ -41,6 +50,15 @@ export class ShowDiseasesPage {
 
   }
 
+  toggleDetails(diseaseNames) {
+    if (diseaseNames.showDetails) {
+      diseaseNames.showDetails = false;
+      diseaseNames.icon = 'ios-add-circle-outline';
+    } else {
+      diseaseNames.showDetails = true;
+      diseaseNames.icon = 'ios-remove-circle-outline';
+    }
+  }
 
 
   ionViewDidLoad() {
